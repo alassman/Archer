2a7647f3226ed05ad780c969e8b6bb8b8e529069
alassman <alassman@umich.edu>
2016-06-01 12:15:09 -0400

    testing

--- test_query.cpp
+++ test_query.cpp
@@ -23,41 +23,37 @@ int main(int argc, char *argv[])
 
 	//Wait 10 ms before sending another command to device
 	sleepms(10);
-	for(int i = 0; i < 500; ++i) {
-		int com_status1 = device.SetCommand(_S, 1, 100);
+	for(int i = 0; i < 100; ++i) {
+		int com_status1 = device.SetCommand(_S, 1, i);
 		if(com_status1) {
 			cout << "ERROR: " << com_status1 << endl;
 		}
-		int com_status2 = device.SetCommand(_S, 2, 100);
+		int com_status2 = device.SetCommand(_S, 2, i);
 		if(com_status2) {
 			cout << "ERROR: " << com_status2 << endl;
 		}
-
-
 		//Check RPM
 
 		int rpm1;
-		cout << "GetValue(_S, 1, rpm1)" << endl;
 		if((status = device.GetValue(_S, 1, rpm1)) != RQ_SUCCESS)
 			cout<<"failed --> "<<status<<endl;
 		else
-			cout<<"returned --> "<<endl;
+			cout << "rpm for motor 1: " << rpm1 << endl;
 
 		//Wait 10 ms before sending another command to device
 		sleepms(10);
 
 		int rpm2;
-		cout << "GetValue(_S, 2, rpm2)" << endl;
 		if((status = device.GetValue(_S, 2, rpm2)) != RQ_SUCCESS)
 			cout<<"failed --> "<<status<<endl;
 		else
-			cout<<"returned --> "<<endl;
+			cout << "rpm for motor 2: " << rpm2 << endl;
 
 		//Wait 10 ms before sending another command to device
 		sleepms(10);
 
-		cout << "rpm for motor 1: " << rpm1 << endl;
-		cout << "rpm for motor 2: " << rpm2 << endl;
+		
+		
 	}	
 	sleepms(10);
 	// for(int i = 0; i < 500; ++i) {
