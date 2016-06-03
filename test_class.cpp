8919d28c6c01c9cc7c6c2c9e2e9061375e36b408
alassman <alassman@umich.edu>
2016-06-03 15:10:05 -0400

    testing

--- test_class.cpp
+++ test_class.cpp
@@ -12,11 +12,11 @@ using namespace std;
 int main() {
 	//Archer(float period, float track, float encoderScaleFactor);
 	Archer robot(10, 2, 300);
-	// vector<int> Mcommand;
-	// Mcommand.push_back(-100);
-	// Mcommand.push_back(100);
+	vector<int> Mcommand;
+	Mcommand.push_back(-100);
+	Mcommand.push_back(100);
 
-	// robot.setActuators(Mcommand);
+	robot.setActuators(Mcommand);
 
 	return 0;
 }
