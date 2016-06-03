7150fb101c42049db7fbd00606cd711d4fd3213f
alassman <alassman@umich.edu>
2016-06-03 15:33:29 -0400

    testing

--- test_class.cpp
+++ test_class.cpp
@@ -10,8 +10,14 @@ using namespace std;
 
 
 int main() {
+/*
 	//Archer(float period, float track, float encoderScaleFactor);
-	Archer robot(10, 2, 300);
+	track = 14", 35.5cm
+	gear ratio = 1:71
+		4*71 = 284
+	period = 10hz
+*/
+	Archer robot(10, 14, 284);
 	vector<int> Mcommand;
 	Mcommand.push_back(-100);
 	Mcommand.push_back(100);
@@ -20,7 +26,7 @@ int main() {
 	for (int i = 0; i < 100; ++i)
 	{
 		robot.setActuators(Mcommand);
-			robot.readSensors();
+		robot.readSensors();
 
 
 	}
