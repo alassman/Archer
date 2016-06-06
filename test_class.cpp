f15465e280593b36470aae0d1a41c8372cb52f6d
alassman <alassman@umich.edu>
2016-06-06 12:03:21 -0400

    testing

--- test_class.cpp
+++ test_class.cpp
@@ -21,46 +21,17 @@ int main() {
 */
 	Archer robot(10, 14, 1800);
 
-
-	// robot.read_encoders();
-	// cout << robot.read_encoders() << endl;
-
-	// sleep(12);
-
-	// cout << robot.read_encoders() << endl;
-
-
-
-
-
-
 	vector<int> Mcommand(2,0);
 	Mcommand[0] = 100;
 	Mcommand[1] = 100;
 
-	double total_disp = 0;
-	robot.readSensors();
-
 	for (int i = 0; i < 40; ++i)
 	{
 		robot.setActuators(Mcommand);
-		total_disp += robot.readSensors();
+		robot.readSensors();
 		cout << total_disp << endl;
 	}
 
-	Mcommand[0] = 0;
-	Mcommand[1] = 0;
-
-	for (int i = 0; i < 150; ++i)
-	{
-		robot.setActuators(Mcommand);
-		total_disp += robot.readSensors();
-		cout << total_disp << endl;
-
-	}
-
-
-	cout << "total displacement: " << total_disp << endl;
 
 
 
